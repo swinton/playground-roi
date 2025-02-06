5cac0247b6fa8c0147a93dbe984a65d89e33348f
Steve Winton <stevewinton@gmail.com>
2025-02-06 15:05:13 -0600

    Use mathjs to contain evil

--- evil.js
+++ evil.js
@@ -1,6 +1,9 @@
+const math = require('mathjs');
 const expression = process.argv.slice(2).join(' ').concat(';');
+
 try {
-  console.log(eval(expression));
+  const result = math.evaluate(expression);
+  console.log(result);
 } catch {
   console.log(`Error: Unable to evaluation ${expression}. Maybe try that again?`);
   process.exit(1);
